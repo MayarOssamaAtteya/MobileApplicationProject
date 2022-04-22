8bb5351771f78675c3d9fd06d19901e1d3cd2d80
mayar <mayaratteya@gmail.com>
2022-04-22 17:01:42 +0200

    mobile app project

---
+++ app/src/main/java/com/example/afinal/skincare.java
@@ -0,0 +1,14 @@
+package com.example.afinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class skincare extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.skin_care);
+    }
+}
