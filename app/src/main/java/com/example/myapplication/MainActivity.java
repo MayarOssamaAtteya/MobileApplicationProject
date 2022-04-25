aac0fe206e7ddf8dcae7934251d3e754477e7519
Marzok16 <102658122+Marzok16@users.noreply.github.com>
2022-04-25 14:27:20 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                openlogin();
+            }
+
+        });
+        button = (Button) findViewById(R.id.button3);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View n){
+                opensignup();
+            }
+
+        });
+    }
+    public void openlogin(){
+        Intent intent=new Intent(this, login.class );
+        startActivity(intent);
+    }
+    public void opensignup(){
+        Intent intent=new Intent(this, signup.class );
+        startActivity(intent);
     }
 }
