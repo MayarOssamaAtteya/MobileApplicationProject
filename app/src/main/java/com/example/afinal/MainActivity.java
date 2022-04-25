f03e9507d2fb2055b92ecc4a76cddd4b918c5e8f
MayarOssamaAtteya <102699327+MayarOssamaAtteya@users.noreply.github.com>
2022-04-26 01:03:19 +0200

    mobile app project

--- app/src/main/java/com/example/afinal/MainActivity.java
+++ app/src/main/java/com/example/afinal/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.marz);
 
         button = (Button) findViewById(R.id.button2);
         button.setOnClickListener(new View.OnClickListener() {
