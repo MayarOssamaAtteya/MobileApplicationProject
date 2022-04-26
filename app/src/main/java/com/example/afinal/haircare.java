e2d00d85c849355a05fc20b12964a03edd7f25f3
Lailaashraff <lailaashraf323@gmail.com>
2022-04-26 03:27:01 +0200

    mobile app project

--- app/src/main/java/com/example/afinal/haircare.java
+++ app/src/main/java/com/example/afinal/haircare.java
@@ -8,6 +8,6 @@ public class haircare extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.haircare);
+        setContentView(R.layout.hair_care);
     }
 }
