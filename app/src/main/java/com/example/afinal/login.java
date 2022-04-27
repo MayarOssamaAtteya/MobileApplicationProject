3cf3cef5120d1d9f9de55170ad7a9a90244f3941
DELL <abdulrhmanmarzok16@gmail.com>
2022-04-27 03:14:06 +0200

    mar

--- app/src/main/java/com/example/afinal/login.java
+++ app/src/main/java/com/example/afinal/login.java
@@ -1,16 +1,113 @@
 package com.example.afinal;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class login extends AppCompatActivity {
+    private EditText user, email;
+    Button button;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        user = findViewById(R.id.textViewu);
+        email = findViewById(R.id.textViewp);
+
+
+
+
+        button = findViewById(R.id.login_login);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("My Notification", "My Notification", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+
+        }
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                /////////notification code//////
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(login.this, "My Notification");
+                builder.setContentTitle("my title");
+                builder.setContentText("welecom to neutralcare ");
+                builder.setSmallIcon(R.drawable.ic_launcher_background);
+                builder.setAutoCancel(true);
+
+                NotificationManagerCompat managerCompat = NotificationManagerCompat.from(login.this);
+                managerCompat.notify(1, builder.build());
+
+                opensignup();
+
+
+
+            }
+        });
+
+
+
+
+
+        };
+
+
+
+    protected void onResume() {
+        super.onResume();
+        //fetching the stored data from the sharedPreference
+        SharedPreferences sh = getSharedPreferences("MySharedPref", MODE_PRIVATE);
+        //the value will be default as empty string
+        //bydwar 3ala el keys fl file b asamehom
+        String uname = sh.getString("username", "");
+        String uemail = sh.getString("email", "");
+
+        int numP = sh.getInt("mobile", 0);
+        //setting the fetched data in the EditTexts
+        user.setText(uname);
+        email.setText(uemail);
+
+
+
     }
+
+
+    protected void onPause() {
+        super.onPause();
+        //basame esm el file ely hahot feh l data
+        //preferred to have the same name as the package
+        //lw 3amalt mode public ay app tany momken y-access 3aleh
+        SharedPreferences sp = getSharedPreferences("MySharedPref", MODE_PRIVATE);
+        //Creating an editor object to edit in the file
+        //permit the system to edit that file and write data in it
+        //ya3ne bafa3al el editor
+        SharedPreferences.Editor myEdit = sp.edit();
+        //store data as key value pair
+        myEdit.putString("username", user.getText().toString());
+        myEdit.putString("email", email.getText().toString());
+        myEdit.apply();
+        //keda ana 3malt store w h3ml retrieve mn el onresume
+    }
+
+
+    public void opensignup(){
+        Intent intent=new Intent(this,  home.class );
+        startActivity(intent);}
 }
+
--- app/src/main/java/com/example/afinal/signup.java
+++ app/src/main/java/com/example/afinal/signup.java
@@ -16,27 +16,29 @@ import android.widget.EditText;
 
 public class signup extends AppCompatActivity {
 
-    private EditText user,email,num,pass,repass;
+    private EditText user, email, num, pass, repass;
     Button not,intentbutton;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
 
-        user=findViewById(R.id.UserInp);
-        email=findViewById(R.id.emailInp);
-        num=findViewById(R.id.NumberInp);
-        pass=findViewById(R.id.passInp);
-        repass=findViewById(R.id.repassinp);
+        user = findViewById(R.id.UserInp);
+        email = findViewById(R.id.emailInp);
+        num = findViewById(R.id.NumberInp);
+        pass = findViewById(R.id.passInp);
+        repass = findViewById(R.id.repassinp);
 
 
-        not=findViewById(R.id.button);
+        not = findViewById(R.id.button);
 
 
-        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
-            NotificationChannel channel=new NotificationChannel("My Notification" ,"My Notification", NotificationManager.IMPORTANCE_DEFAULT);
-            NotificationManager manager=getSystemService(NotificationManager.class);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("My Notification", "My Notification", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getSystemService(NotificationManager.class);
             manager.createNotificationChannel(channel);
 
         }
@@ -48,36 +50,41 @@ public class signup extends AppCompatActivity {
             public void onClick(View v) {
 
                 /////////notification code//////
-                NotificationCompat.Builder builder=new NotificationCompat.Builder(signup.this,"My Notification");
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(signup.this, "My Notification");
                 builder.setContentTitle("my title");
                 builder.setContentText("welecom to neutralcare ");
                 builder.setSmallIcon(R.drawable.ic_launcher_background);
                 builder.setAutoCancel(true);
 
-                NotificationManagerCompat managerCompat=NotificationManagerCompat.from(signup.this);
-                managerCompat.notify(1,builder.build());
+                NotificationManagerCompat managerCompat = NotificationManagerCompat.from(signup.this);
+                managerCompat.notify(1, builder.build());
                 Intent intent = new Intent(getApplicationContext(),home.class);
 //                String text=data.getText().toString();
 //                intent.putExtra("string",text);
                 startActivity(intent);
+
+                    open();
+
+
+                    open();
+
 
 
             }
         });
     }
 
-    protected void onResume()
-    {
+    protected void onResume() {
         super.onResume();
         //fetching the stored data from the sharedPreference
-        SharedPreferences sh= getSharedPreferences("MySharedPref",MODE_PRIVATE);
+        SharedPreferences sh = getSharedPreferences("MySharedPref", MODE_PRIVATE);
         //the value will be default as empty string
         //bydwar 3ala el keys fl file b asamehom
-        String uname=sh.getString("username","");
-        String uemail=sh.getString("email","");
-        String upass=sh.getString("password","");
-        String urepass=sh.getString("reenter","");
-        int numP =sh.getInt("mobile",0);
+        String uname = sh.getString("username", "");
+        String uemail = sh.getString("email", "");
+        String upass = sh.getString("password", "");
+        String urepass = sh.getString("reenter", "");
+        int numP = sh.getInt("mobile", 0);
         //setting the fetched data in the EditTexts
         user.setText(uname);
         email.setText(uemail);
@@ -88,23 +95,22 @@ public class signup extends AppCompatActivity {
     }
 
 
-    protected void onPause()
-    {
+    protected void onPause() {
         super.onPause();
         //basame esm el file ely hahot feh l data
         //preferred to have the same name as the package
         //lw 3amalt mode public ay app tany momken y-access 3aleh
-        SharedPreferences sp = getSharedPreferences("MySharedPref",MODE_PRIVATE);
+        SharedPreferences sp = getSharedPreferences("MySharedPref", MODE_PRIVATE);
         //Creating an editor object to edit in the file
         //permit the system to edit that file and write data in it
         //ya3ne bafa3al el editor
-        SharedPreferences.Editor myEdit =sp.edit();
+        SharedPreferences.Editor myEdit = sp.edit();
         //store data as key value pair
-        myEdit.putString("username",user.getText().toString());
-        myEdit.putString("email",email.getText().toString());
-        myEdit.putString("password",pass.getText().toString());
-        myEdit.putString("reenter",repass.getText().toString());
-        myEdit.putInt("mobile",Integer.parseInt(num.getText().toString()));
+        myEdit.putString("username", user.getText().toString());
+        myEdit.putString("email", email.getText().toString());
+        myEdit.putString("password", pass.getText().toString());
+        myEdit.putString("reenter", repass.getText().toString());
+        myEdit.putInt("mobile", Integer.parseInt(num.getText().toString()));
         //apply 3shan yekteb fl file
         myEdit.apply();
         //keda ana 3malt store w h3ml retrieve mn el onresume
@@ -112,6 +118,12 @@ public class signup extends AppCompatActivity {
 
 
 
+
+
+    public void open() {
+        Intent intent = new Intent(this, home.class);
+        startActivity(intent);
+    }
 
 
 }
