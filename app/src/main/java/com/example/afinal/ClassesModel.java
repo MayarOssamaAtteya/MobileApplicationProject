0beb4cd4f69dad1cb0b7d12ca5300255dccac9a0
Omar Akram <Omarakram302@gmail.com>
2022-04-28 03:42:42 +0200

    hair care latest

---
+++ app/src/main/java/com/example/afinal/ClassesModel.java
@@ -0,0 +1,24 @@
+package com.example.afinal;
+
+public class ClassesModel {
+    String name;
+
+    int image;
+
+    public ClassesModel(String name, int image) {
+        this.name = name;
+
+        this.image = image;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+
+
+    public int getImage() {
+        return image;
+    }
+}
--- app/src/main/java/com/example/afinal/haircare.java
+++ app/src/main/java/com/example/afinal/haircare.java
@@ -1,27 +1,46 @@
 package com.example.afinal;
 
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageView;
 
-public class haircare extends AppCompatActivity implements View.OnClickListener {
-    Button cart;
+import com.example.afinal.R;
+
+import java.util.ArrayList;
+
+public class haircare extends AppCompatActivity {
+    ArrayList<ClassesModel> classesModels = new ArrayList<>();
+    int[] classesImages={R.drawable.asd,R.drawable.img1,R.drawable.secich, R.drawable.img2,R.drawable.aveeno,R.drawable.giovanni};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hair_care);
 
-        cart=findViewById(R.id.button6);
-        cart.setOnClickListener(this);
+        RecyclerView recyclerView=findViewById(R.id.mRecyclerView);
+
+        steUpClassesModels();
+
+        programAdapter adapter=new programAdapter(this,classesModels);
+
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+    }
+    private void steUpClassesModels(){
+        String[] classesNames=getResources().getStringArray(R.array.classes_names);
+        for(int i =0;i<classesNames.length;i++){
+            classesModels.add(new ClassesModel(classesNames[i],classesImages[i]));
+
+        }
     }
 
-    @Override
-    public void onClick(View view) {
-        Intent intent=new Intent(this,mody.class);
-        startActivity(intent);
-    }
 }
